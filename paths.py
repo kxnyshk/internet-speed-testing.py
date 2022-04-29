ee3c4371a197c20e2f80a3c47d7e6798373f4a1b
kxnyshk <ikanishk1172@gmail.com>
2022-04-30 01:56:16 +0530

    added Static/ paths

---
+++ paths.py
@@ -0,0 +1,10 @@
+# PATHS
+PATH_FETCH = './Static/fetch.txt'
+PATH_BEST = './Static/best.txt'
+PATH_FOUND = './Static/found.txt'
+PATH_TEST = './Static/test.txt'
+PATH_DOWN = './Static/download.txt'
+PATH_UPLD = './Static/upload.txt'
+
+# MODE
+MODE = 'r'
